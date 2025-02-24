9368b5956dc7944779be863d9e1d3e96f89f9401
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-25 00:54:59 +0600

    added main.html file

--- js/login.js
+++ js/login.js
@@ -1,5 +1,7 @@
 const loginbtn = document.getElementById('btn_login');
 
 loginbtn.addEventListener('click', (event)=>{
-    
+    window.location.href = './view/main.html';
+    event.preventDefault();
+
 })
