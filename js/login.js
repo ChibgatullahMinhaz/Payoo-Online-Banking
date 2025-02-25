23051fb5dfbbf609d4f3e384d037c2a5863d4921
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-26 00:16:32 +0600

    handle condition login and logout

--- js/login.js
+++ js/login.js
@@ -1,7 +1,18 @@
-const loginbtn = document.getElementById('btn_login');
+document.getElementById("btnlogin").addEventListener('click', (event)=>{
 
-loginbtn.addEventListener('click', (event)=>{
-    window.location.href = './view/main.html';
+   const acounNumber = document.getElementById('AcNumber').value.trim();
+   const acountPIN = parseInt(document.getElementById("AcountPIN").value);
+  console.log(acounNumber.length);
+   if (acounNumber.length === 11 ) {
+    if (acountPIN === 1234) {
+        window.location.href = '../view/main.html';
+    }
+    else{
+        alert("tik nei ")
+    }
+   }else{
+    alert("not okky ")
+   }
     event.preventDefault();
 
 })
---
+++ js/main.js
@@ -0,0 +1,5 @@
+// handle log out 
+const logout = document.getElementById('logoutbtn');
+logout.addEventListener('click', (e)=>{
+    window.location.href = '../index.html'
+})
